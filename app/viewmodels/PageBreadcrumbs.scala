afdfb868e8439dcf8cba6491207f43165e96868e
Jack Brannon <166420733+Jack-Brannon@users.noreply.github.com>
2026-07-21 12:39:06 +0100

    Dtr 6795 (#3)
    * Tax Transcations Page + Tests
    
    * DTR-6795 | Tax Transactions Page + Tests
    
    * DTR-6795 | Tax Transactions Page + Tests
    
    * DTR-6795 | Tax Transactions Page + Tests
    
    * DTR-6795 | Tax Transactions Page + Tests
    
    * DTR-6796 | Added total to table
    
    * DTR-6795 | Added taxDescription variable passed to view, will be refactored out under BF20 DTR-6797

---
+++ app/viewmodels/PageBreadcrumbs.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels
+
+import uk.gov.hmrc.govukfrontend.views.viewmodels.breadcrumbs.{Breadcrumbs, BreadcrumbsItem}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+
+object PageBreadcrumbs {
+
+  def taxTransactionsPage: Breadcrumbs = Breadcrumbs(
+    // TODO: Change these to be from message file (are these page titles?)
+    // TODO: Add hrefs
+    items = Seq(
+      BreadcrumbsItem(content = Text("Corporation tax home"), href = Some("/")),
+      BreadcrumbsItem(content = Text("Balance"), href = Some("/")),
+      BreadcrumbsItem(content = Text("Accounting period overview"), href = Some("/"))
+    )
+  )
+
+}
