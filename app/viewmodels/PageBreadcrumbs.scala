fa9cc0dfdd136bfde3728b6c368d1d20b0b80849
avelx <3528155+avelx@users.noreply.github.com>
2026-09-15 15:53:49 +0100

    DTR-6389: apply changes as per PR review comment

--- app/viewmodels/PageBreadcrumbs.scala
+++ app/viewmodels/PageBreadcrumbs.scala
@@ -72,7 +72,7 @@ object PageBreadcrumbs {
       BreadcrumbsItem(content = Text(messages("breadcrumbs.home")), href = Some("/")),
       BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriods")), href = Some("/")),
       BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriodEnding")), href = Some("/")),
-      BreadcrumbsItem(content = Text(messages("breadcrumbs.debitInterest")), href = Some("/"))
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.interest")), href = Some("/"))
     )
   )
 
