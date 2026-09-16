8db167b40652611a3d0f23b4afdc51101c288692
jack-brannon <166420733+Jack-Brannon@users.noreply.github.com>
2026-09-16 23:17:09 +0100

    DTR-6391 | Credit Interest drill down page and tests

--- app/viewmodels/PageBreadcrumbs.scala
+++ app/viewmodels/PageBreadcrumbs.scala
@@ -19,6 +19,7 @@ package viewmodels
 import uk.gov.hmrc.govukfrontend.views.viewmodels.breadcrumbs.{Breadcrumbs, BreadcrumbsItem}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
 import play.api.i18n.Messages
+import controllers.routes
 
 object PageBreadcrumbs {
 
@@ -63,6 +64,19 @@ object PageBreadcrumbs {
       BreadcrumbsItem(content = Text(messages("breadcrumbs.home")), href = Some("/")),
       BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriods")), href = Some("/")),
       BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriodEnding")), href = Some("/"))
+    )
+  )
+
+  def creditInterestPage(implicit messages: Messages): Breadcrumbs = Breadcrumbs(
+    // TODO: Add hrefs
+    items = Seq(
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.home")), href = Some("/")),
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriods")), href = Some("/")),
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriodEnding")), href = Some("/")),
+      BreadcrumbsItem(
+        content = Text(messages("breadcrumbs.interest")),
+        href = Some(routes.InterestController.onPageLoad().url)
+      )
     )
   )
 }
--- test/viewmodels/PageBreadcrumbsSpec.scala
+++ test/viewmodels/PageBreadcrumbsSpec.scala
@@ -22,6 +22,7 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.breadcrumbs.BreadcrumbsItem
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
 import play.api.i18n.Messages
 import play.api.test.Helpers.stubMessages
+import controllers.routes
 
 class PageBreadcrumbsSpec extends AnyWordSpec with Matchers {
 
@@ -70,6 +71,26 @@ class PageBreadcrumbsSpec extends AnyWordSpec with Matchers {
         BreadcrumbsItem(content = Text(messages("breadcrumbs.home")), href = Some("/")),
         BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriods")), href = Some("/")),
         BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriodEnding")), href = Some("/"))
+      )
+    }
+
+  }
+
+  "PageBreadcrumbs.creditInterestPage" should {
+
+    "contain exactly four breadcrumb items" in {
+      PageBreadcrumbs.creditInterestPage.items.size shouldBe 4
+    }
+
+    "have the correct items in order" in {
+      PageBreadcrumbs.creditInterestPage.items shouldBe Seq(
+        BreadcrumbsItem(content = Text(messages("breadcrumbs.home")), href = Some("/")),
+        BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriods")), href = Some("/")),
+        BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriodEnding")), href = Some("/")),
+        BreadcrumbsItem(
+          content = Text(messages("breadcrumbs.interest")),
+          href = Some(routes.InterestController.onPageLoad().url)
+        )
       )
     }
 
